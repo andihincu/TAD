e6f554aa637da2cc417e7e087baafb2c43d5b97c
andihincu <andihincu>
2020-11-23 18:30:18 +0100

    TAD

--- joan.js
+++ joan.js
@@ -19,8 +19,8 @@ Persona.prototype.getCognom = function(cognomNou){
 return this.congnom;
 }
 
-let traballador = new Persona("Pau","Vicario");
-let treballador2 = new Persona("Ismael", "Barroso")
+let traballador = new Persona("Andi","Hincu");
+let treballador2 = new Persona("Albert", "Hincu")
 
 treballador.saluda();
 
